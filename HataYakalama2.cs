3734a7c7707ba74e83195b1a28574039492c06ad
emirhan <erxn1d1110@gmail.com>
2025-01-09 20:05:44 +0300

    Form1

--- HataYakalama2.cs
+++ HataYakalama2.cs
@@ -1,22 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Hata_Yakalama2
 {
-    internal static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// Uygulamanın ana girdi noktası.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
         }
     }
 }
