593a1d81cce1b8f578ea65f9136099ef542e6d22
emirhan <erxn1d1110@gmail.com>
2025-01-09 20:05:59 +0300

    Form1

--- HesapMakinesi.cs
+++ HesapMakinesi.cs
@@ -1,22 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Hesap_Makinesi
 {
-    internal static class Program
+    public partial class Form1 : Form
     {
-        /// <summary>
-        /// Uygulamanın ana girdi noktası.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        public Form1()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            InitializeComponent();
+        }
+        int sayi1, sayi2;
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int çıkar;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox1.Text);
+            çıkar = sayi1 - sayi2;
+            textBox3.Text = çıkar.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int çarp;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox1.Text);
+            çarp = sayi1 * sayi2;
+            textBox3.Text = çarp.ToString();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int kareal;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox1.Text);
+            kareal= sayi1 * sayi1;
+            textBox3.Text = kareal.ToString();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int böl;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox1.Text);
+            böl = sayi1 / sayi2;
+            textBox3.Text = böl.ToString();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int modal;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox1.Text);
+            modal = sayi1 % sayi2;
+            textBox3.Text = modal.ToString();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int topla;
+            sayi1 = Convert.ToInt16(textBox1.Text);
+            sayi2 = Convert.ToInt16(textBox1.Text);
+            topla = sayi1 + sayi2;
+            textBox3.Text = topla.ToString();
         }
     }
 }
